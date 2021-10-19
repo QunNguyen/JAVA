9511b65ef27ec9f9babec3bf2d1b2a287e53c4e9
Your Name <you@example.com>
2021-10-19 09:37:56 +0700

    update

--- src/thuchanh2/bai4/giangviencohuu.java
+++ src/thuchanh2/bai4/giangviencohuu.java
@@ -1,6 +1,15 @@
 package thuchanh2.bai4;
 
 
+import java.util.Scanner;
+
 public class giangviencohuu extends giangvien{
     private String sogioquydinh;
+
+    @Override
+    public void nhap(String s) {
+        super.nhap(s);
+        Scanner sc=new Scanner(System.in);
+        sogioquydinh=sc.nextLine();
+    }
 }
