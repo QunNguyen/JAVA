dc1e48d1fe941b0919efcf1d87d7e02ad43c6998
Your Name <you@example.com>
2021-10-19 09:42:29 +0700

    update

---
+++ src/README.txt
@@ -0,0 +1,4 @@
+kiem tra trang thai: git status
+them file bo nho dem: git add .
+day file len bo nho dem: git commit -m "update"
+day file len local: git push
--- src/thuchanh2/bai4/giangviencohuu.java
+++ src/thuchanh2/bai4/giangviencohuu.java
@@ -1,15 +1,8 @@
 package thuchanh2.bai4;
 
 
-import java.util.Scanner;
 
 public class giangviencohuu extends giangvien{
     private String sogioquydinh;
 
-    @Override
-    public void nhap(String s) {
-        super.nhap(s);
-        Scanner sc=new Scanner(System.in);
-        sogioquydinh=sc.nextLine();
-    }
 }
